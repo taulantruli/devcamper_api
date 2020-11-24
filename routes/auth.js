f2d3bfd02b1215a25f41dd569f17b7560485c329
Taulant <taulant7ruli@gmail.com>
2020-11-24 21:03:14 +0100

    Reset Password and Update User Details

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,9 @@ const {
   login,
   getMe,
   forgotPassword,
+  resetPassword,
+  updateDetails,
+  updatePassword,
 } = require("../controllers/auth");
 
 const router = express.Router();
@@ -12,7 +15,10 @@ const { protect } = require("../middleware/auth");
 
 router.post("/register", register);
 router.post("/login", login);
-router.post("/forgotpassword", forgotPassword);
 router.get("/me", protect, getMe);
+router.put("/updatedetails", protect, updateDetails);
+router.put("/updatepassword", protect, updatePassword);
+router.post("/forgotpassword", forgotPassword);
+router.put("/resetpassword/:resettoken", resetPassword);
 
 module.exports = router;
